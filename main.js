783ca0ea4c4806db49a048987ff00791227ae5de
emestabillo <emsestabillo@gmail.com>
2019-07-08 21:03:26 -0400

    Nav responsive, toggle logic

---
+++ main.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById("main-menu");
+let navBarToggle = document.getElementById("toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
